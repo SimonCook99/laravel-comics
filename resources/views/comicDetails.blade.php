afb8cc54212848e0033c0c84b64df8f1b4736e63
Simone Mauro <simon.mauro@virgilio.it>
2022-03-30 15:48:11 +0200

    ultimi ritocchi, esercizio completato

--- resources/views/comicDetails.blade.php
+++ resources/views/comicDetails.blade.php
@@ -85,6 +85,31 @@
                 <hr>
             </div>
         </div>
+        
+        <nav>
+            <ul>
+                <li>
+                    <img src="{{asset("images/buy-comics-digital-comics.png") }}" alt="digital comics">
+                    <p>digital comics</p>
+                </li>
+                <li>
+                    <img src="{{asset("images/buy-comics-merchandise.png") }}" alt="merchandise">
+                    <p>DC merchandise</p>
+                </li>
+                <li>
+                    <img src="{{asset("images/buy-comics-subscriptions.png") }}" alt="subscriptions">
+                    <p>Subscription</p>
+                </li>
+                <li>
+                    <img src="{{asset("images/buy-comics-shop-locator.png") }}" alt="shop locator">
+                    <p>comic shop locator</p>
+                </li>
+                <li>
+                    <img src="{{asset("images/buy-dc-power-visa.svg") }}" alt="dc power visa">
+                    <p>DC power visa</p>
+                </li>
+            </ul>
+        </nav>
     </div>
     
 
